8b28daa0c8ccca6dc0b0af710d72b6e2729ef219
Todd Griffin <todd.griffin61@outlook.com>
2023-11-15 22:05:14 +0000

    added styling to hello element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,13 @@ import palms from '../../public/palm_tree_sunset.webp'
 export default function Home() {
   return (
     <>
-      <div 
-        className='bg-palm-trees height-screen
-          bg-no-repeat bg-cover bg-center bg-fixed h-screen flex justify-center items-center'
-      >
-        <div
-          className=''
-        ><h1
-            className='font-bold text-6xl'
-          >Hello!</h1>
+      <div
+        className='bg-palm-trees height-screen bg-no-repeat bg-cover bg-center
+        bg-fixed h-screen flex justify-center items-center' >
+        <div className='bg-gray-600 bg-opacity-20 p-6 rounded-md' >
+          <h1 className='font-bold text-6xl text-white' >
+            Hello!
+          </h1>
         </div>
       </div>
     </>

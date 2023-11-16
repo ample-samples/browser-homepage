336f1d2bf9579483dcd4dd0c831062f7f80f66e5
Todd Griffin <todd.griffin61@outlook.com>
2023-11-16 11:02:48 +0000

    added neat transition with reac-collapsible

--- src/app/components/quickLink.tsx
+++ src/app/components/quickLink.tsx
@@ -9,7 +9,6 @@ interface Props {
 export function QuickLink(params: Props) {
   return (
     <a href={params.href} >
-        {params.src}
       <Image
         className='filter brightness-0 invert hover:h-12 hover:w-12 hover:p-1 hover:p-1 rounded-none hover:rounded-2xl transition-all'
           src={params.src}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,19 @@ import Image from 'next/image'
 import palms from '../../public/palm_tree_sunset.webp'
 import Clock from 'react-live-clock';
 import { QuickLink } from './components/quickLink'
+import Collapsible from 'react-collapsible';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const quickLinkProps ={
+  const quickLinkProps = {
     size: 48,
   }
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    setIsOpen(true)
+  }, [])
 
   return (
     <>
@@ -15,18 +23,22 @@ export default function Home() {
         className='bg-palm-trees height-screen bg-no-repeat bg-cover bg-center
         bg-fixed h-screen flex justify-center items-center' >
         <div className='bg-gray-600 bg-opacity-20 py-9 px-14 rounded-md text-center' >
-          <h1 className='font-bold text-8xl mb-7' >
-            Hello!
-          </h1>
-          <p className='text-3xl'>
-            The time is:
-          </p>
-          <Clock className='text-6xl' format={'HH:mm:ss'} ticking={true} />
-          <div className='mt-10 flex space-between gap-x-4 justify-center items-center content-center w-full h-12' >
-            <QuickLink href='https://www.github.com' src="/assets/github-mark-white.svg"   size={quickLinkProps.size} />
-            <QuickLink href='https://www.youtube.com' src="/assets/youtube-svgrepo-com.svg" size={quickLinkProps.size} />
-            <QuickLink href='https://www.linkedin.com' src="/assets/iconmonstr-linkedin-3.svg" size={quickLinkProps.size} />
-          </div>
+          <Collapsible open={!isOpen} easing="ease-in-out">
+            <h1 className='font-bold text-8xl' >
+              Hello!
+            </h1>
+          </Collapsible>
+          <Collapsible open={isOpen} easing="ease-in-out">
+            <p className='text-3xl'>
+              The time is:
+            </p>
+            <Clock className='text-6xl' format={'HH:mm:ss'} ticking={true} />
+            <div className='mt-10 flex space-between gap-x-4 justify-center items-center content-center w-full h-12' >
+              <QuickLink href='https://www.github.com' src="/assets/github-mark-white.svg" size={quickLinkProps.size} />
+              <QuickLink href='https://www.youtube.com' src="/assets/youtube-svgrepo-com.svg" size={quickLinkProps.size} />
+              <QuickLink href='https://www.linkedin.com' src="/assets/iconmonstr-linkedin-3.svg" size={quickLinkProps.size} />
+            </div>
+          </Collapsible>
         </div>
       </div>
     </>

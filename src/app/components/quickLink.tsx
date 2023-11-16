9776121c4ff7fd7f1a9bf9575cc170a45c31117f
Todd Griffin <todd.griffin61@outlook.com>
2023-11-16 00:15:16 +0000

    removed clsx and fixed css jitter bug

--- src/app/components/quickLink.tsx
+++ src/app/components/quickLink.tsx
@@ -10,7 +10,7 @@ export function QuickLink(params: Props) {
   return (
     <a href={params.href} >
       <Image
-        className='filter brightness-0 invert hover:h-10 hover:w-10 transition-all'
+        className='filter brightness-0 invert hover:h-12 hover:w-12 hover:p-1 hover:p-1 rounded-none hover:rounded-2xl transition-all'
           src={params.src}
           alt="GitHub logo"
           width={params.size || 50}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,12 @@ import Image from 'next/image'
 import palms from '../../public/palm_tree_sunset.webp'
 import Clock from 'react-live-clock';
 import { QuickLink } from './components/quickLink'
-import clsx from 'clsx'
 
 export default function Home() {
   const quickLinkProps ={
-    size: 50,
+    size: 48,
   }
 
-  const quickLinkContainerProps = {
-    size: `h-[${quickLinkProps.size + 80}px]`
-  }
   return (
     <>
       <div
@@ -22,11 +18,11 @@ export default function Home() {
           <h1 className='font-bold text-8xl mb-7' >
             Hello!
           </h1>
-          <p className='text-3xl' >
+          <p className='text-3xl'>
             The time is:
           </p>
           <Clock className='text-6xl' format={'HH:mm:ss'} ticking={true} />
-          <div className={clsx('mt-10 flex space-between gap-x-4 justify-center items-center content-center w-full', quickLinkContainerProps.size)}>
+          <div className='mt-10 flex space-between gap-x-4 justify-center items-center content-center w-full h-12' >
             <QuickLink href='https://www.github.com' src="/assets/github-mark-white.svg"   size={quickLinkProps.size} />
             <QuickLink href='https://www.youtube.com' src="/assets/youtube-svgrepo-com.svg" size={quickLinkProps.size} />
             <QuickLink href='https://www.linkedin.com' src="/assets/iconmonstr-linkedin-3.svg" size={quickLinkProps.size} />
